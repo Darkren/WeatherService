05ebf7a26c2b87199de0518dd86803ee30ae0d7f
darkrengarius <darkren92@gmail.com>
2018-09-13 15:18:35 +0300

    Fix main.go

--- main.go
+++ main.go
@@ -1,50 +1,5 @@
 package main
 
-import (
-	"WeatherService/config"
-	"fmt"
-)
-
 func main() {
-	test := `{
-		"id": 1,
-		"name": "qwerty",
-		"birthday": "12.09.2018",
-		"address": 
-		{
-			"city": "Moscow",
-			"street": "Lenina str."
-		}
-	}`
 
-	configuration, err := config.New(test)
-	if err != nil {
-		panic(err)
-	}
-
-	name, err := configuration.GetString("name")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Name: %v\n", name)
-
-	id, err := configuration.GetInt("id")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Id: %v\n", id)
-
-	addressSection, err := configuration.Section("address")
-	if err != nil {
-		panic(err)
-	}
-
-	city, err := addressSection.GetString("city")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("City: %v\n", city)
 }
