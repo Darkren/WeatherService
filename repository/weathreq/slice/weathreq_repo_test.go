0a6a2cfa51503f5c0ec8b858ca7b4d4590122565
darkrengarius <darkren92@gmail.com>
2018-09-14 13:36:40 +0300

    Finish tests on slice repo implementations

--- repository/weathreq/slice/weathreq_repo_test.go
+++ repository/weathreq/slice/weathreq_repo_test.go
@@ -19,11 +19,167 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Got err on creating repo")
 	}
 
-	tests := []*models.WeatherRequest{}
+	tests := []struct {
+		req    *models.WeatherRequest
+		wantID int64
+	}{
+		{
+			req: &models.WeatherRequest{
+				Lat: 15.46,
+				Lon: 13.45,
+			},
+			wantID: 1,
+		},
+		{
+			req: &models.WeatherRequest{
+				Lat: 19.28,
+				Lon: 10.287,
+			},
+			wantID: 2,
+		},
+		{
+			req: &models.WeatherRequest{
+				Lat: 12.22222,
+				Lon: 90.87,
+			},
+			wantID: 3,
+		},
+	}
 
-	id, err := repo.Add()
+	for _, test := range tests {
+		id, err := repo.Add(test.req)
+		if err != nil {
+			t.Errorf("Got err inserting %v", test.req)
+		}
+
+		if id != test.wantID {
+			t.Errorf("Got %v, expected %v", id, test.wantID)
+		}
+	}
 }
 
 func TestGetNotComplete(t *testing.T) {
+	repo := New()
+	if repo == nil {
+		t.Errorf("Got err on creating repo")
+	}
 
+	tests := []struct {
+		req    *models.WeatherRequest
+		wantID int64
+	}{
+		{
+			req: &models.WeatherRequest{
+				Lat: 15.46,
+				Lon: 13.45,
+			},
+			wantID: 1,
+		},
+		{
+			req: &models.WeatherRequest{
+				Lat: 19.28,
+				Lon: 10.287,
+			},
+			wantID: 2,
+		},
+		{
+			req: &models.WeatherRequest{
+				Lat: 12.22222,
+				Lon: 90.87,
+			},
+			wantID: 3,
+		},
+	}
+
+	for _, test := range tests {
+		id, err := repo.Add(test.req)
+		if err != nil {
+			t.Errorf("Got err inserting %v", test.req)
+		}
+
+		if id != test.wantID {
+			t.Errorf("Got %v, expected %v", id, test.wantID)
+		}
+	}
+
+	for _, test := range tests {
+		test.req.IsComplete = true
+	}
+
+	notComplete, err := repo.GetNotComplete()
+	if err != nil {
+		t.Error("Got err on GetNotComplete")
+	}
+
+	if notComplete != nil {
+		t.Errorf("Got %v, expected none", notComplete)
+	}
+
+	tests[1].req.IsComplete = false
+
+	notComplete, err = repo.GetNotComplete()
+	if err != nil {
+		t.Error("Got err on GetNotComplete")
+	}
+
+	if notComplete == nil {
+		t.Error("Got none")
+	}
+	if notComplete.IsComplete {
+		t.Errorf("Got %v, IsComplete should be false", notComplete)
+	}
+}
+
+func TestSetComplete(t *testing.T) {
+	repo := New()
+	if repo == nil {
+		t.Errorf("Got err on creating repo")
+	}
+
+	tests := []struct {
+		req    *models.WeatherRequest
+		wantID int64
+	}{
+		{
+			req: &models.WeatherRequest{
+				Lat: 15.46,
+				Lon: 13.45,
+			},
+			wantID: 1,
+		},
+		{
+			req: &models.WeatherRequest{
+				Lat: 19.28,
+				Lon: 10.287,
+			},
+			wantID: 2,
+		},
+		{
+			req: &models.WeatherRequest{
+				Lat: 12.22222,
+				Lon: 90.87,
+			},
+			wantID: 3,
+		},
+	}
+
+	for _, test := range tests {
+		id, err := repo.Add(test.req)
+		if err != nil {
+			t.Errorf("Got err inserting %v", test.req)
+		}
+
+		if id != test.wantID {
+			t.Errorf("Got %v, expected %v", id, test.wantID)
+		}
+	}
+
+	err := repo.SetComplete(3)
+	if err != nil {
+		t.Error("Got err in SetComplete")
+	}
+
+	if !tests[2].req.IsComplete {
+		t.Error("IsComplete should be true")
+	}
 }
---
+++ repository/weathresp/slice/weathresp_repo_test.go
@@ -0,0 +1,125 @@
+package slice
+
+import (
+	"testing"
+
+	"github.com/Darkren/weatherservice/models"
+)
+
+func TestNew(t *testing.T) {
+	repo := New()
+	if repo == nil {
+		t.Errorf("Got err on creating repo")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	repo := New()
+	if repo == nil {
+		t.Errorf("Got err on creating repo")
+	}
+
+	tests := []struct {
+		resp   *models.WeatherResponse
+		wantID int64
+	}{
+		{
+			resp: &models.WeatherResponse{
+				Humidity:    45,
+				Pressure:    34,
+				RequestID:   1,
+				Temperature: 27,
+			},
+			wantID: 1,
+		},
+		{
+			resp: &models.WeatherResponse{
+				Humidity:    23,
+				Pressure:    11,
+				RequestID:   2,
+				Temperature: 22,
+			},
+			wantID: 2,
+		},
+		{
+			resp: &models.WeatherResponse{
+				Humidity:    87,
+				Pressure:    34,
+				RequestID:   3,
+				Temperature: 20,
+			},
+			wantID: 3,
+		},
+	}
+
+	for _, test := range tests {
+		id, err := repo.Add(test.resp)
+		if err != nil {
+			t.Errorf("Got err inserting %v", test.resp)
+		}
+
+		if id != test.wantID {
+			t.Errorf("Got %v, expected %v", id, test.wantID)
+		}
+	}
+}
+
+func TestByRequestID(t *testing.T) {
+	repo := New()
+	if repo == nil {
+		t.Errorf("Got err on creating repo")
+	}
+
+	tests := []struct {
+		resp   *models.WeatherResponse
+		wantID int64
+	}{
+		{
+			resp: &models.WeatherResponse{
+				Humidity:    45,
+				Pressure:    34,
+				RequestID:   1,
+				Temperature: 27,
+			},
+			wantID: 1,
+		},
+		{
+			resp: &models.WeatherResponse{
+				Humidity:    23,
+				Pressure:    11,
+				RequestID:   2,
+				Temperature: 22,
+			},
+			wantID: 2,
+		},
+		{
+			resp: &models.WeatherResponse{
+				Humidity:    87,
+				Pressure:    34,
+				RequestID:   3,
+				Temperature: 20,
+			},
+			wantID: 3,
+		},
+	}
+
+	for _, test := range tests {
+		id, err := repo.Add(test.resp)
+		if err != nil {
+			t.Errorf("Got err inserting %v", test.resp)
+		}
+
+		if id != test.wantID {
+			t.Errorf("Got %v, expected %v", id, test.wantID)
+		}
+	}
+
+	got, err := repo.GetByRequestID(2)
+	if err != nil {
+		t.Error("Got err in GetByRequestID")
+	}
+
+	if got.RequestID != 2 {
+		t.Error("Got wrong response")
+	}
+}
