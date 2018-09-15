8acfdca2a8c25d7e8aa6b5fe7f3b737fb0dca213
Sir Darkrengarius <darkren92@gmail.com>
2018-09-15 21:14:21 +0300

    Prepare weather request and its repo for total safety
    Added field IsInProgress to mark it as one being processed
    
    Changed repo methods to clearly show which side effects
    they have

--- repository/weathreq_repo.go
+++ repository/weathreq_repo.go
@@ -5,6 +5,6 @@ import "github.com/Darkren/weatherservice/models"
 // WeatherRequestRepository is a storage of WeatherRequest
 type WeatherRequestRepository interface {
 	Add(req *models.WeatherRequest) (int64, error)
-	GetNotComplete() (*models.WeatherRequest, error)
-	SetComplete(id int64) error
+	GetForProcessing() (*models.WeatherRequest, error)
+	ProcessingFinished(id int64) error
 }
